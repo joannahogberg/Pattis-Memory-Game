bca3cb6fb909b89216b8d2d7e8e9c5e1ce52677a
Joanna Högberg <joanna.hogberg@gmail.com>
2017-12-21 22:56:41 +0100

    updated styles

--- script.js
+++ script.js
@@ -120,6 +120,13 @@ function startGame() {
 
     var nrArr = ["0", "0", "1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10", "11", "11", "12", "12", "13", "13", "14", "14", "15", "15", "16", "16", "17", "17", "18", "18"]; //Array för alla bilder
     picsArr = nrArr.filter(picsNr => picsNr < brickElems.length / 2);
+
+    if (brickElems.length > 20) {
+        for (i = 0; i < brickElems.length; i++) {
+            brickElems[i].style.width = "15%";
+            brickElems[i].style.height = "15%";
+        }
+    }
     console.log(picsArr)
     startGameBtn.disabled = true; // Inaktiverar startGameBtn då nytt spel startas
     formElem.disabled = true; // Inaktiverar menyn för att välja antal brickor då nytt spel startats
